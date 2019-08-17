a86623605bf54c6da7e5a723596a93469f30b0a9
Tom Hanika (sys:companion) <hanika@cs.uni-kassel.de>
2019-08-17 13:51:51 +0200

    teg-rest: More examples

--- teg-rest/src/teg_rest/testcases.clj
+++ teg-rest/src/teg_rest/testcases.clj
@@ -325,4 +325,233 @@
     (println body)))
 
 
+(defn test-rw3-s1 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P272" "P17" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
 
+
+;; [20/15007 (#{"country (P17)" "shares border with (P47)" "named after (P138)"}
+;; ⟶ #{"located in the administrative territorial entity (P131)"})]
+(defn test-rw4-s1 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P47" "P17" "P138" "P131"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+;; [18/15007 (#{"capital (P36)" "named after (P138)"
+;;              "located in the administrative territorial entity (P131)"}
+;; ⟶ #{"country (P17)"})]
+(defn test-rw5-s1 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P36" "P138" "P131" "P17"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+;; [32/45021 (#{"country of citizenship (P27)" "member of (P463)"
+;;              "field of work (P101)" "instance of (P31)"}
+;;            ⟶ #{"occupation (P106)"})]
+(defn test-rw6-s1 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P27" "P463" "P101" "P31" "P106"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+
+;; [29/45021 (#{"series (P179)" "publisher (P123)"} ⟶ #{"genre (P136)"})]
+(defn test-rw7-s1 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P179" "P123" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+(defn test-rw7-s2 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P179" "P123" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       "Q2110870" ["P123"]
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+(defn test-rw7-s3 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P179" "P123" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       "Q2110870" ["P123"]
+                                                       "Q333563" ["P136"]
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+
+(defn test-rw7-s4 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P179" "P123" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       "Q2110870" ["P123"]
+                                                       "Q333563" ["P136"]
+                                                       "Q9349" ["P136" "P123"]
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+(defn test-rw7-s5 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P179" "P123" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       "Q2110870" ["P123"]
+                                                       "Q333563" ["P136"]
+                                                       "Q9349" ["P136" "P123"]
+                                                       "Q207542" ["P179"]
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "10"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+(defn test-rw7-s6 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P179" "P123" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       "Q2110870" ["P123"]
+                                                       "Q333563" ["P136"]
+                                                       "Q9349" ["P136" "P123"]
+                                                       "Q207542" ["P179"]
+                                                       "Q18091411" ["P179" "P123"]
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
+
+(defn test-rw7-s7 [] ;; real world test from previous experiments
+  (let [{:keys [status headers body error] :as resp}
+        @(http/get "http://127.0.0.1:4223/explore"
+                   {:query-params
+                    {"sessionID" 987654321
+                     "properties" ["P179" "P123" "P136"],
+                     :counterexamples (json/write-str {
+                                                       "Qartificial" []
+                                                       "Q2110870" ["P123"]
+                                                       "Q333563" ["P136"]
+                                                       "Q9349" ["P136" "P123"]
+                                                       "Q207542" ["P179"]
+                                                       "Q18091411" ["P179" "P123"]
+                                                       "Q1110652" ["P179" "P136"]
+                                                       }),
+                     :implications [
+                                    ],
+                     "maxCounterexamples" "5"
+                     }})]
+    (if error
+      (println "Failed, exception: " error)
+      (println "HTTP GET success: " status))
+    (println body)))
