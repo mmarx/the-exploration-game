ddee613983352fd43826dd28c73c6f7a0e96835b
Tom Hanika (sys:companion) <hanika@cs.uni-kassel.de>
2019-10-14 20:23:05 +0200

    Added about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,57 @@
+<style scoped lang="less">
+  li {
+  font-size:30px
+  }
+</style>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div>
+  <h1>The Exploration Game is proudly presented by:</h1>
+  <ul>
+    <li>
+      <a href="https://kbs.inf.tu-dresden.de">Knowledge-Based Systems Group,
+        TU Dresden, Germany</a>
+    </li>
+    <li>
+      <a href="https://www.kde.cs.uni-kassel.de">Knowledge & Data Engineering
+        Group (KDE), University of Kassel, Germany</a>
+    </li>
+  </ul>
+  <h3>You may contact for questions or research inquiries:</h3>
+  <ul>
+    <li>
+      <a href="https://kbs.inf.tu-dresden.de/max">Dipl.-Math. Maximilian
+      Marx</a><br />
+      or
+    </li>
+    <li>
+      <a href="https://www.kde.cs.uni-kassel.de/hanika">Dr. Tom Hanika</a>
+    </li>
+  </ul>
+  <h3>The relevant research works for TEG can be found here: </h3>
+  <ul>
+    <li>
+      Knowledge-Based Systems Group, TU Dresden, Germany</a> Hanika, T., Marx,
+M., Stumme,
+G.: <a href="https://doi.org/10.1007/978-3-030-21462-3_21">Discovering
+  Implicational Knowledge in Wikidata.</a> In: Cristea, D., Ber, F.L., and
+Sertkaya,
+B. (eds.) <a href="https://link.springer.com/book/10.1007/978-3-030-21462-3">ICFCA’19</a>.
+pp. 315–323. Springer (2019).
+</li>
+<li>Hanika, T., Marx, M., Stumme,
+  G.: <a href="http://arxiv.org/abs/1902.00916">Discovering Implicational
+    Knowledge in
+    Wikidata</a>, <a href="http://arxiv.org/abs/1902.00916">http://arxiv.org/abs/1902.00916</a>,
+  (2019).
+</li>
+<li>
+  <a href="https://commons.wikimedia.org/wiki/File:The_Exploration_Game.pdf">The
+  Exploration Game</a> (Poster
+  at <a href="https://wikimania.wikimedia.org/wiki/Wikimania">Wikimania
+  2019</a>, August 2019)
+</li>
+</ul>
+</div>
 </template>
+
