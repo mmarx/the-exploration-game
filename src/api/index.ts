bb8a15a8b6dff4985c10b7e225b1c9bca93364da
Maximilian Marx <maximilian.marx@tu-dresden.de>
2019-12-06 19:10:28 +0100

    Move query for the number of counterexamples to the client

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,9 @@ import { Atom, Implication, EntityId, Counterexamples,
          ExplorationResult, ExplorationRequest } from '~/api/types'
 
 import { http } from '@/http'
+import { sparqlRequest } from '@/api'
+import { SparqlBinding } from '@/api/types'
+import { sparqlEndpoint } from '@/api/endpoints'
 
 const backendEndpoint = '/teg/api/'
 
@@ -31,12 +34,84 @@ export async function getNextExplorationStep(
   return response
 }
 
-export async function getNumberOfCounterexamples(implication: Implication,
-                                                 sessionId: string) {
-  const response = await performApiRequest('howmany', {
-    implication,
-    sessionId,
-  } as any)
+export async function sparqlQuery(query: string): Promise<SparqlBinding[]> {
+  const response = await sparqlRequest(sparqlEndpoint,
+                                       `#TOOL:TEG, https://tools.wmflabs.org/teg/
+${query}`)
+  return response.results.bindings
+}
 
-  return (response as any).numberOfCounterexamples as number || 0
+export type CounterExampleQueryType = AskType | LimitedCounterExampleQueryType
+
+export interface AskType {
+  type: 'ask'
+}
+
+export interface LimitedCounterExampleQueryType {
+  type: 'count' | 'retrieve'
+  limit?: number
+}
+
+interface CountResult {
+  entities: { datatype: string,
+              type: 'literal',
+              value: number,
+            },
+}
+
+export function counterExampleQueryForImplication(implication: Implication,
+                                                  queryType: CounterExampleQueryType) {
+  let verb
+  let limit
+
+  switch (queryType.type) {
+    case 'ask':
+      verb = 'ASK'
+      break
+
+    case 'count':
+      verb = 'SELECT (COUNT(?entity) AS ?entities)'
+      limit = ((queryType.limit !== null)
+               ? `LIMIT ${queryType.limit}`
+               : '')
+      break
+
+    case 'retrieve':
+      verb = 'SELECT ?entity'
+      limit = ((queryType.limit !== null)
+               ? `LIMIT ${queryType.limit}`
+               : '')
+      break
+  }
+
+  const premise = patternForPremise(implication.head)
+  const conclusion = patternForConclusion(implication.body)
+
+  return `${verb} WHERE {
+  ${premise}
+  ${conclusion}
+} ${limit}`
+}
+
+function patternForPremise(body: Atom[]) {
+  if (!body.length) {
+    return `?entity ?someProperty [] .
+[] wikibase:directClaim ?someProperty`
+  }
+
+  const clauses = body.map((property) => `wdt:${property} []`).join(';\n    ')
+
+  return `?entity
+${clauses} .`
+}
+
+function patternForConclusion(head: Atom[]) {
+  return head.map((property) => `{ FILTER NOT EXISTS { ?entity wdt:${property} [] . } }`).join(' UNION\n  ')
+}
+
+export async function getNumberOfCounterexamples(implication: Implication,
+                                                 _: string) {
+  const response = await sparqlQuery(counterExampleQueryForImplication(implication,
+                                                                       { type: 'count', limit: 1000 }))
+  return ((response[0] as any) as CountResult).entities.value || 0
 }
